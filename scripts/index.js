f0e4fa593e87c91de4edafed785f762cf6816611
vich3er <cherednikviktoriya@gmail.com>
2024-12-07 12:52:49 +0200

    del???

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let ul = document.querySelector('ul');
+let select = document.querySelector('select');
 let form = document.forms.addValue;
 let input = document.forms.addValue.addValueInp;
 let deleteBtn = document.getElementById('delete');
@@ -9,24 +9,21 @@ nonePar.innerText = "you haven't added anything yet";
 let incorrectPar = document.createElement('p');
 incorrectPar.innerText = "incorrect input format";
 
+// console.log(document.getElementsByClassName('p-ul')[1]);
+
+
 let checkAndTrim = () => {
     input.value = input.value.trim();// хз чи тут треба трім
     if (input.value.includes("=")) {
         let [name, value] = input.value.split('=');
         name = name.trim()
         value = value.trim()
-        if  (!!!(name.match(/\W+/g) && !!!(value.match( /\W+/g)))) {
-           // form.appendChild(incorrectPar);
-           // return false
-           //  console.log(!!name.match(/\W+/g), name);
-           //  console.log(!!value.match( /\W+/g), value);
-            console.log(name);
-            return [name, value]
-        } else {
+        if  (!!(name.match(/[^a-zA-Z0-9-а-яА-Я0-9-іІ_]+/g) || !!(value.match(  /[^a-zA-Z0-9-а-яА-Я0-9-іІ_]+/g)))) {
             form.appendChild(incorrectPar);
             return  false
+        } else {
+            return [name, value]
         }
-
     } else {
         form.appendChild(incorrectPar);
         return false;
@@ -36,59 +33,112 @@ let checkAndTrim = () => {
 function Input(name, value) {
     this.name = name.trim();
     this.value = value.trim();
+    this.id =  inpId()
+
+
+}
+let inpId = id()
+function id(){
+
+    let outputList = JSON.parse(localStorage.getItem("outputList")) || []
+    let start = outputList.length;
+    return function (){
+        return ++start
+    }
+
 }
 
 form.onsubmit = function (e) {
     e.preventDefault();
     if (checkAndTrim()) {
         //  ul.style.display = block;
-        ul.hidden = false;
+        select.hidden = false;
         let outputList = JSON.parse(localStorage.getItem('outputList')) || [];
         nonePar.remove();
         incorrectPar.remove();
         let [name, value] = checkAndTrim();
         // let [name, value] = input.value.split("=");
         outputList.push(new Input(name, value));
-        let li = document.createElement('li');
-        li.innerText = `${name}=${value}`
+        let option = document.createElement('option');
+         option.value = `${name}=${value}`
+        option.innerText=     `${name}=${value}`
         localStorage.setItem("outputList", JSON.stringify(outputList));
         input.value = '';
-        ul.appendChild(li);
+        select.appendChild(option);
     }
 }
 
 let showList = (outputList) => {
     if (outputList.length > 0) {
-        ul.innerText = "";
-        ul.hidden = false;
+        select.innerHTML = "";
+       // console.log(select.options);
+        select.hidden = false;
         console.log(outputList);
         //  let ul = document.createElement('ul');
         for (el of outputList) {
-            let [name, value] = Object.values(el);
-            let li = document.createElement('li');
-            li.innerText = `${name}=${value}`
-            ul.appendChild(li);
+            let [name, value, id] = Object.values(el);
+            let option = document.createElement('option');
+            // option.value = `${name}=${value}`
+            option.value = `${name}=${value} ${id}`
+            option.innerText = `${name}=${value}`
+            select.appendChild(option);
 
         }
         // nameSortBtn.before(ul);
-        document.getElementsByClassName('p-ul')[1].after(ul);
+        //console.log(document.getElementsByClassName('p-ul')[1]);
+      //  document.getElementsByClassName('p-ul')[1].after(select);
     } else {
-        ul.hidden = true;
-        ul.before(nonePar);
+        select.hidden = true;
+        select.before(nonePar);
 
     }
 }
 
+
+
+
+
 let outputList = JSON.parse(localStorage.getItem("outputList")) || []
 showList(outputList);
 
+select.onchange=()=>{
+    let selectedItems = Array.from(select.value);
+}
+
+let selectedItems = select.onchange=()=>{
+   return selectedItems = select.selectedOptions;
+}
+
 deleteBtn.onclick = () => {
     if (localStorage.getItem('outputList')) {
-        localStorage.removeItem('outputList');
-        ul.innerText = "";
-        ul.before(nonePar);
+        // localStorage.removeItem('outputList');
+        let outputList = JSON.parse(localStorage.getItem("outputList"))
 
-        ul.hidden = true;
+            // console.log(selectedItems)
+            console.log(selectedItems);
+            for (selId of selectedItems){
+               for (el of outputList){
+                   if (selId.id ==el.id) {
+                       console.log(selId.id, el.id);
+                       console.log(el);
+                       outputList.splice(el);
+                       console.log(outputList);
+                   }
+               }
+
+                console.log(outputList.length);
+                if (outputList.length===0) localStorage.removeItem("outputList", outputList)
+            localStorage.setItem("outputList", outputList);
+            console.log(outputList);
+            showList(outputList);
+
+
+        }
+
+
+        // select.innerHTML = "";
+        // select.before(nonePar);
+        // select.hidden = true;
     }
 }
 
@@ -101,7 +151,7 @@ valueSortBtn.onclick = () => {
             if (a.value < b.value) return -1;
             else return 0;
         })
-        console.log(sortByValue);
+       // console.log(sortByValue);
         showList(outputList);
 
     }
@@ -121,3 +171,4 @@ nameSortBtn.onclick = () => {
 
     }
 }
+
